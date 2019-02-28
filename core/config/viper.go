0593687bad315a0515d4187772c03f5b24129286
Камиль Мухаметзянов <grkamil@yandex.ru>
2019-02-28 12:24:44 +0300

    use viper as config

---
+++ core/config/viper.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"github.com/MinterTeam/minter-explorer-api/helpers"
+	"github.com/spf13/viper"
+	"strings"
+)
+
+type Config interface {
+	GetString(key string) string
+	GetInt(key string) int
+	GetBool(key string) bool
+	Init()
+}
+
+type viperConfig struct {
+}
+
+func (v *viperConfig) Init() {
+	viper.AutomaticEnv()
+
+	viper.AddConfigPath(".")
+
+	replacer := strings.NewReplacer(`.`, `_`)
+	viper.SetEnvKeyReplacer(replacer)
+	viper.SetConfigType(`json`)
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+
+	//panic
+	helpers.CheckErr(err)
+}
+
+func (v *viperConfig) GetString(key string) string {
+	return viper.GetString(key)
+}
+
+func (v *viperConfig) GetInt(key string) int {
+	return viper.GetInt(key)
+}
+
+func (v *viperConfig) GetBool(key string) bool {
+	return viper.GetBool(key)
+}
+
+func NewViperConfig() Config {
+	v := &viperConfig{}
+	v.Init()
+	return v
+}
--- core/enviroment.go
+++ core/enviroment.go
@@ -1,8 +1,7 @@
 package core
 
 import (
-	"flag"
-	"os"
+	"github.com/MinterTeam/minter-explorer-api/core/config"
 )
 
 type Environment struct {
@@ -13,37 +12,13 @@ type Environment struct {
 }
 
 func NewEnvironment() *Environment {
-	dbName := flag.String("db_name", "", "DB name")
-	dbUser := flag.String("db_user", "", "DB user")
-	dbPassword := flag.String("db_password", "", "DB password")
-	baseCoin := flag.String("base_coin", "", "Base Coin")
-	flag.Parse()
+	cfg := config.NewViperConfig()
 
 	env := Environment{
-		DbName:     *dbName,
-		DbUser:     *dbUser,
-		DbPassword: *dbPassword,
-		BaseCoin:   *baseCoin,
-	}
-
-	if env.DbUser == "" {
-		dbUser := os.Getenv("EXPLORER_DB_USER")
-		env.DbUser = dbUser
-	}
-
-	if env.DbName == "" {
-		dbName := os.Getenv("EXPLORER_DB_NAME")
-		env.DbName = dbName
-	}
-
-	if env.DbPassword == "" {
-		dbPassword := os.Getenv("EXPLORER_DB_PASSWORD")
-		env.DbPassword = dbPassword
-	}
-
-	if env.BaseCoin == "" {
-		baseCoin := os.Getenv("EXPLORER_BASE_COIN")
-		env.BaseCoin = baseCoin
+		DbName:     cfg.GetString("database.name"),
+		DbUser:     cfg.GetString("database.user"),
+		DbPassword: cfg.GetString("database.password"),
+		BaseCoin:   cfg.GetString("baseCoin"),
 	}
 
 	return &env
