8ae78f476ae47384e4260da1b4d68a2399b83b5d
Камиль Мухаметзянов <grkamil@yandex.ru>
2019-03-21 13:45:18 +0300

    add check data to redeem check resource

--- coins/resource.go
+++ coins/resource.go
@@ -6,20 +6,20 @@ import (
 )
 
 type Resource struct {
-	Crr            uint64 `json:"crr"`
+	Crr uint64 `json:"crr"`
 	//Volume         string `json:"volume"` TODO: return when fields will be correct
 	//ReserveBalance string `json:"reserveBalance"` TODO: return when fields will be correct
-	Name           string `json:"name"`
-	Symbol         string `json:"symbol"`
+	Name   string `json:"name"`
+	Symbol string `json:"symbol"`
 }
 
 func (Resource) Transform(model resource.ItemInterface, params ...interface{}) resource.Interface {
 	coin := model.(models.Coin)
 	return Resource{
-		Crr:            coin.Crr,
+		Crr: coin.Crr,
 		//Volume:         helpers.PipStr2Bip(coin.Volume),
 		//ReserveBalance: helpers.PipStr2Bip(coin.ReserveBalance),
-		Name:           coin.Name,
-		Symbol:         coin.Symbol,
+		Name:   coin.Name,
+		Symbol: coin.Symbol,
 	}
 }
--- transaction/data_resources/redeem_check.go
+++ transaction/data_resources/redeem_check.go
@@ -1,13 +1,25 @@
 package data_resources
 
 import (
+	"encoding/base64"
+	"github.com/MinterTeam/minter-explorer-api/helpers"
 	"github.com/MinterTeam/minter-explorer-api/resource"
 	"github.com/MinterTeam/minter-explorer-tools/models"
+	"github.com/MinterTeam/minter-go-node/core/check"
 )
 
 type RedeemCheck struct {
-	RawCheck string `json:"raw_check"`
-	Proof    string `json:"proof"`
+	RawCheck string    `json:"raw_check"`
+	Proof    string    `json:"proof"`
+	Data     CheckData `json:"data"`
+}
+
+type CheckData struct {
+	Coin     string `json:"coin"`
+	Nonce    uint64 `json:"nonce"`
+	Value    string `json:"value"`
+	Sender   string `json:"sender"`
+	DueBlock uint64 `json:"due_block"`
 }
 
 func (RedeemCheck) Transform(txData resource.ItemInterface, params ...interface{}) resource.Interface {
@@ -16,5 +28,20 @@ func (RedeemCheck) Transform(txData resource.ItemInterface, params ...interface{
 	return RedeemCheck{
 		RawCheck: data.RawCheck,
 		Proof:    data.Proof,
+		Data:     TransformCheckData(data.RawCheck),
+	}
+}
+
+func TransformCheckData(raw string) CheckData {
+	decoded, _ := base64.StdEncoding.DecodeString(raw)
+	data, _ := check.DecodeFromBytes(decoded)
+	sender, _ := data.Sender()
+
+	return CheckData{
+		Coin:     data.Coin.String(),
+		Nonce:    data.Nonce,
+		Value:    helpers.PipStr2Bip(data.Value.String()),
+		Sender:   sender.String(),
+		DueBlock: data.DueBlock,
 	}
 }
