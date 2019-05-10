3408b3ee9ed7c3c3ea98cad34f4ad3ce45598d89
Камиль Мухаметзянов <grkamil@yandex.ru>
2019-05-10 13:47:07 +0300

    use db host from config

--- database/main.go
+++ database/main.go
@@ -12,6 +12,7 @@ func Connect(env *core.Environment) *pg.DB {
 		Password: env.DbPassword,
 		Database: env.DbName,
 		PoolSize: env.DbPoolSize,
+		Addr:     env.DbHost,
 	}
 
 	db := pg.Connect(options)
