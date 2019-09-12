91cb8546b62eed6ceee0a2a3232a9d607ff8780c
Камиль Мухаметзянов <grkamil@yandex.ru>
2019-09-12 13:30:19 +0300

    WA-212: move validators meta information to meta resource

--- validator/resource.go
+++ validator/resource.go
@@ -9,14 +9,18 @@ import (
 
 type Resource struct {
 	Status         *uint8               `json:"status"`
-	Name           *string              `json:"name"`
-	Description    *string              `json:"description"`
-	IconUrl        *string              `json:"icon_url"`
-	SiteUrl        *string              `json:"site_url"`
+	Meta           MetaResource         `json:"meta"`
 	Stake          *string              `json:"stake"`
 	Part           *string              `json:"part"`
 	DelegatorCount int                  `json:"delegator_count"`
 	DelegatorList  []resource.Interface `json:"delegator_list"`
+}
+
+type MetaResource struct {
+	Name        *string `json:"name"`
+	Description *string `json:"description"`
+	IconUrl     *string `json:"icon_url"`
+	SiteUrl     *string `json:"site_url"`
 }
 
 // Required extra params: activeValidatorIds and totalStake.
@@ -42,13 +46,15 @@ func (r Resource) Transform(model resource.ItemInterface, params ...interface{})
 
 	return Resource{
 		Status:         validator.Status,
-		Name:           validator.Name,
-		Description:    validator.Description,
-		IconUrl:        validator.IconUrl,
-		SiteUrl:        validator.SiteUrl,
 		Stake:          stake,
 		Part:           part,
 		DelegatorCount: len(delegators),
 		DelegatorList:  delegators,
+		Meta: MetaResource{
+			Name:        validator.Name,
+			Description: validator.Description,
+			IconUrl:     validator.IconUrl,
+			SiteUrl:     validator.SiteUrl,
+		},
 	}
 }
