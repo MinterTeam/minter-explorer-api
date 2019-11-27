411eb9e318aa4540fc81fd36e376c11caaf009b3
Kamil Mukhametzyanov <grkamil@yandex.ru>
2019-11-27 17:23:23 +0300

    fix

--- transaction/data_resources/create_multisig.go
+++ transaction/data_resources/create_multisig.go
@@ -6,8 +6,8 @@ import (
 )
 
 type CreateMultisig struct {
-	Threshold uint64   `json:"threshold"`
-	Weights   []uint64 `json:"weights"`
+	Threshold string   `json:"threshold"`
+	Weights   []string `json:"weights"`
 	Addresses []string `json:"addresses"`
 }
 
