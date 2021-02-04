6267c6660fd2510dff18d163f565e2fb06328713
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-02-04 11:43:53 +0300

    fix

--- api/v2/pools/routes.go
+++ api/v2/pools/routes.go
@@ -6,7 +6,7 @@ import "github.com/gin-gonic/gin"
 func ApplyRoutes(r *gin.RouterGroup) {
 	pools := r.Group("/pools")
 	{
-		pools.GET("/", GetSwapPools)
+		pools.GET("", GetSwapPools)
 		pools.GET("/:coin0/:coin1", GetSwapPool)
 		pools.GET("/:coin0/:coin1/:address", GetSwapPoolProvider)
 	}
