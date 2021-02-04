7178b5aaa99ca22e33b8943aaf7183a7306d95ea
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-02-04 13:25:43 +0300

    fix

--- pool/service.go
+++ pool/service.go
@@ -20,7 +20,7 @@ func (s *Service) GetPoolLiquidityInBip(pool models.LiquidityPool) *big.Int {
 		secondCoinInBip = helpers.StringToBigInt(pool.SecondCoinVolume)
 	)
 
-	if pool.FirstCoinId != 0 {
+	if pool.FirstCoinId != 0 && pool.FirstCoin.Crr != 0 {
 		firstCoinInBip = formula.CalculateSaleReturn(
 			helpers.StringToBigInt(pool.FirstCoin.Volume),
 			helpers.StringToBigInt(pool.FirstCoin.Reserve),
@@ -29,7 +29,7 @@ func (s *Service) GetPoolLiquidityInBip(pool models.LiquidityPool) *big.Int {
 		)
 	}
 
-	if pool.SecondCoinId != 0 {
+	if pool.SecondCoinId != 0 && pool.SecondCoin.Crr != 0 {
 		secondCoinInBip = formula.CalculateSaleReturn(
 			helpers.StringToBigInt(pool.SecondCoin.Volume),
 			helpers.StringToBigInt(pool.SecondCoin.Reserve),
