989d0d7a95fb2f1688e7f24286d4a85eb71faa8d
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-05-21 12:17:17 +0300

    add routines to resource transform

--- resource/main.go
+++ resource/main.go
@@ -1,7 +1,9 @@
 package resource
 
 import (
+	"github.com/MinterTeam/minter-explorer-api/v2/errors"
 	"reflect"
+	"sync"
 )
 
 type ItemInterface interface{}
@@ -15,8 +17,14 @@ func TransformCollection(collection interface{}, resource Interface) []Interface
 	models := makeItemsFromModelsCollection(collection)
 	result := make([]Interface, len(models))
 
+	wg := &sync.WaitGroup{}
 	for i := range models {
-		result[i] = resource.Transform(models[i])
+		wg.Add(1)
+		go func(i int, wg *sync.WaitGroup) {
+			defer wg.Done()
+			defer errors.Recovery()
+			result[i] = resource.Transform(models[i])
+		}(i, wg)
 	}
 
 	return result
@@ -26,9 +34,16 @@ func TransformCollectionWithCallback(collection interface{}, resource Interface,
 	models := makeItemsFromModelsCollection(collection)
 	result := make([]Interface, len(models))
 
+	wg := &sync.WaitGroup{}
 	for i := range models {
-		result[i] = resource.Transform(models[i], callbackFunc(models[i])...)
+		wg.Add(1)
+		go func(i int, wg *sync.WaitGroup) {
+			defer wg.Done()
+			defer errors.Recovery()
+			result[i] = resource.Transform(models[i], callbackFunc(models[i])...)
+		}(i, wg)
 	}
+	wg.Wait()
 
 	return result
 }
