db79978dea1f8dec34c820b22aa163f686ddc006
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-07-28 10:53:04 +0300

    fix

--- cmd/explorer.go
+++ cmd/explorer.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"github.com/MinterTeam/minter-explorer-api/v2/api"
 	"github.com/MinterTeam/minter-explorer-api/v2/core"
 	"github.com/MinterTeam/minter-explorer-api/v2/core/ws"
