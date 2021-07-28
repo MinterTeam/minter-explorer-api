aa8b89f8f4d74f168bf34bd8c1d2074f23f531c7
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-07-28 10:56:53 +0300

    fix

--- pool/service.go
+++ pool/service.go
@@ -74,20 +74,18 @@ func (s *Service) FindSwapRoutePath(fromCoinId, toCoinId uint64, tradeType swap.
 }
 
 func (s *Service) FindSwapRoutePathByPools(liquidityPools []models.LiquidityPool, fromCoinId, toCoinId uint64, tradeType swap.TradeType, amount *big.Int) (*swap.Trade, error) {
-	//paths, err := s.findSwapRoutePathsByGraph(liquidityPools, fromCoinId, toCoinId, 5)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//pools, err := s.getPathsRelatedPools(liquidityPools, paths)
-	//if err != nil {
-	//	return nil, err
-	//}
+	paths, err := s.findSwapRoutePathsByGraph(liquidityPools, fromCoinId, toCoinId, 5)
+	if err != nil {
+		return nil, err
+	}
 
-	var err error
+	pools, err := s.getPathsRelatedPools(liquidityPools, paths)
+	if err != nil {
+		return nil, err
+	}
 
 	pairs := make([]swap.Pair, 0)
-	for _, p := range liquidityPools {
+	for _, p := range pools {
 		pairs = append(pairs, swap.NewPair(
 			swap.NewTokenAmount(swap.NewToken(p.FirstCoinId), helpers.StringToBigInt(p.FirstCoinVolume)),
 			swap.NewTokenAmount(swap.NewToken(p.SecondCoinId), helpers.StringToBigInt(p.SecondCoinVolume)),
