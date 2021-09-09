8d802e073cc58450c9d788367ed92d49023f3485
Kamil Mukhametzyanov <grkamil@yandex.ru>
2021-09-09 12:53:16 +0300

    add limit orders to swap tx resources

--- pool/models.go
+++ pool/models.go
@@ -8,11 +8,27 @@ import (
 )
 
 type SwapChain struct {
-	PoolId   uint64 `json:"pool_id"`
-	CoinIn   uint64 `json:"coin_in"`
+	PoolId   int    `json:"pool_id"`
+	CoinIn   int    `json:"coin_in"`
 	ValueIn  string `json:"value_in"`
-	CoinOut  uint64 `json:"coin_out"`
+	CoinOut  int    `json:"coin_out"`
 	ValueOut string `json:"value_out"`
+	Details  struct {
+		AmountIn            string `json:"amount_in"`
+		AmountOut           string `json:"amount_out"`
+		CommissionAmountIn  string `json:"commission_amount_in"`
+		CommissionAmountOut string `json:"commission_amount_out"`
+		Orders              []struct {
+			Buy    string `json:"buy"`
+			Sell   string `json:"sell"`
+			Seller string `json:"seller"`
+			Id     int    `json:"id"`
+		} `json:"orders"`
+	} `json:"details"`
+	Sellers []struct {
+		Seller string `json:"seller"`
+		Value  string `json:"value"`
+	} `json:"sellers"`
 }
 
 type tradeVolume struct {
